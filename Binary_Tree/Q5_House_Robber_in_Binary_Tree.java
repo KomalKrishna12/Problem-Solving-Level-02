404ab63ec60928c01ea8491506962b352010e1c9
KomalKrishna12 <komalkrishna016@gmail.com>
2021-12-28 14:12:55 +0530

    house robbery

--- Binary_Tree/Q5_House_Robber_in_Binary_Tree.java
+++ Binary_Tree/Q5_House_Robber_in_Binary_Tree.java
@@ -13,27 +13,49 @@ public class Q5_House_Robber_in_Binary_Tree {
         }
     }
 
-    public static class HousePair {
-        int withrobbery;
-        int withoutrobbery;
-    }
+    // first approach
+    // this is pair approach
+    // we create a HousePair class having two variables withrobbery and
+    // withoutrobbery
+    // when root is null return new HousePair() {contain 0 in both variables}
+    // left - contain for root.left
+    // right - contain for root.right
+    // now calculate for root
+    // create a HousePair variable myans
+    // myans.withrobbery = root.val + both values in withoutrobbery
+    // myans.withoutrobbery = math.max of with or without for both left and right
+    // child
+    // basically we want to calulate max amount so that we can take any max value
+    // which may e without or with
+    // robbery
+    // public static class HousePair {
+    // int withrobbery;
+    // int withoutrobbery;
+    // }
 
-    public static HousePair HouseRobber_(TreeNode root) {
-        if (root == null)
-            return new HousePair();
-        HousePair left = HouseRobber_(root.left);
-        HousePair right = HouseRobber_(root.right);
+    // public static HousePair HouseRobber_(TreeNode root) {
+    // if (root == null)
+    // return new HousePair();
+    // HousePair left = HouseRobber_(root.left);
+    // HousePair right = HouseRobber_(root.right);
 
-        HousePair myans = new HousePair();
-        myans.withrobbery = root.val + left.withoutrobbery + right.withoutrobbery;
-        myans.withoutrobbery = Math.max(left.withrobbery, left.withoutrobbery) + Math.max(right.withrobbery,
-                right.withoutrobbery);
-        return myans;        
-    }
+    // HousePair myans = new HousePair();
+    // myans.withrobbery = root.val + left.withoutrobbery + right.withoutrobbery;
+    // myans.withoutrobbery = Math.max(left.withrobbery, left.withoutrobbery) +
+    // Math.max(right.withrobbery,
+    // right.withoutrobbery);
+    // return myans;
+    // }
 
+    // public static int HouseRobber(TreeNode root) {
+    // HousePair res = HouseRobber_(root);
+    // return Math.max(res.withrobbery, res.withoutrobbery);
+    // }
+
+    // second approach
+    
     public static int HouseRobber(TreeNode root) {
-        HousePair res = HouseRobber_(root);
-        return Math.max(res.withrobbery, res.withoutrobbery);
+        return 0;
     }
 
     // input_Section_====================================================================
