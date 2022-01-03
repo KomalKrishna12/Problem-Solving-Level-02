734400a3bfbffd352326fa6edefa0206198c9306
KomalKrishna12 <komalkrishna016@gmail.com>
2022-01-03 23:11:57 +0530

    serialize and deserialize binary tree

---
+++ Binary_Tree/Q11_Serialize_and_Deserialize_Binary_Tree.java
@@ -0,0 +1,85 @@
+public class Q11_Serialize_and_Deserialize_Binary_Tree{
+    public static Scanner scn = new Scanner(System.in);
+
+    public static class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    // we use StringBuilder instead of string because when we concatenate string then it'll create 
+    // a copy of previous string then add new string and return new ref
+    // and in stringbuilder it'll directly add new string and return same ref
+    public static void serialize(TreeNode root, StringBuilder sb){
+        if(root == null){
+            sb.append("null,");
+            return;
+        }
+        sb.append(root.val + ",");
+        serialize(root.left, sb);
+        serialize(root.right, sb);
+    }
+
+    // Encodes a tree to a single string.
+    public static String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        serialize(root, sb);
+        return sb.toString();
+    }
+
+    // Decodes your encoded data to tree.
+    public static TreeNode deserialize(String str) {
+
+    }
+
+    // input_section=================================================
+
+    public static void display(TreeNode node) {
+        if (node == null)
+            return;
+
+        StringBuilder sb = new StringBuilder();
+        sb.append((node.left != null ? node.left.val : "."));
+        sb.append(" -> " + node.val + " <- ");
+        sb.append((node.right != null ? node.right.val : "."));
+
+        System.out.println(sb.toString());
+
+        display(node.left);
+        display(node.right);
+
+    }
+
+    public static TreeNode createTree(int[] arr, int[] IDX) {
+        if (IDX[0] > arr.length || arr[IDX[0]] == -1) {
+            IDX[0]++;
+            return null;
+        }
+        TreeNode node = new TreeNode(arr[IDX[0]++]);
+        node.left = createTree(arr, IDX);
+        node.right = createTree(arr, IDX);
+
+        return node;
+    }
+
+    public static void solve() {
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = scn.nextInt();
+
+        int[] IDX = new int[1];
+        TreeNode root = createTree(arr, IDX);
+
+        String s = serialize(root);
+        display(deserialize(s));
+    }
+
+    public static void main(String[] args) {
+        solve();
+    }
+}
