2fc78c1698ce4266bc743e4ab7771eef61f0bd68
KomalKrishna12 <komalkrishna016@gmail.com>
2022-01-10 13:41:39 +0530

    burn tree

--- Binary_Tree/Q31_Burning_Tree.java
+++ Binary_Tree/Q31_Burning_Tree.java
@@ -14,15 +14,31 @@ public class Q31_Burning_Tree {
 
   static int maxTime = 0;
 
-  public static void burnDown(TreeNode root, TreeNode block, int size) {
+  // if root is null or block then return
+  // update maxTime
+  // call left with time + 1
+  public static void burnDown(TreeNode root, TreeNode block, int time) {
       if(root == null || root == block) return;
 
-      maxTime = Math.max(maxTime, size);
+      maxTime = Math.max(maxTime, time);
 
-      burnDown(root.left, block, size + 1);
-      burnDown(root.right, block, size + 1);
+      burnDown(root.left, block, time + 1);
+      burnDown(root.right, block, time + 1);
   }
 
+  // in this problem we use the same approach of All nodes at distance K
+  // we have given root and fireNode
+  // at fireNode tree start burning so we've to calculate the min time in which whole tree burns
+  // it'll take 1 second to burn it's left or right node
+  // here we use same concept of root to node path
+  // start searching fireNode
+  // if root is null then return -1 that means we've not found the fire node
+  // when root.val == fireNode call burn down and return 1 to it's parent that means burning starts and it'll
+  // burn it's parent in 1 second of time
+  // now check if left burn is not equal to -1 that means tree is burning in left side so call burn down
+  // and pass block node as left becoz left is already burning so return from left and burn right and return
+  // lb + 1 that means left burn in lb time and now add 1
+  // same with right burn and if left and right both is -1 then return -1
   public static int burningTree_(TreeNode root, int fireNode) {
       if(root == null) return -1;
       if(root.val == fireNode){
