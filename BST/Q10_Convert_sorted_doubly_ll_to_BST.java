245f94364228346365fa54b15b34d4400dfe6bee
KomalKrishna12 <komalkrishna016@gmail.com>
2022-01-11 20:41:23 +0530

    sorted doubly linked list to BST

--- BST/Q10_Convert_sorted_doubly_ll_to_BST.java
+++ BST/Q10_Convert_sorted_doubly_ll_to_BST.java
@@ -1,60 +1,92 @@
 import java.util.*;
+
 public class Q10_Convert_sorted_doubly_ll_to_BST {
     public static Scanner scn = new Scanner(System.in);
 
-  public static class Node {
-    int val = 0;
-    Node left = null;
-    Node right = null;
+    public static class Node {
+        int val = 0;
+        Node left = null;
+        Node right = null;
 
-    Node(int val) {
-      this.val = val;
-    }
-  }
-
-  // left : prev, right : next
-  public static Node SortedDLLToBST(Node head) {
-    return null;
-  }
-
-  // Input_code===================================================
-
-  public static void display(Node node) {
-    if (node == null)
-      return;
-
-    StringBuilder sb = new StringBuilder();
-    sb.append((node.left != null ? node.left.val : "."));
-    sb.append(" -> " + node.val + " <- ");
-    sb.append((node.right != null ? node.right.val : "."));
-
-    System.out.println(sb.toString());
-
-    display(node.left);
-    display(node.right);
-
-  }
-
-  public static Node makeList(int n) {
-    Node dummy = new Node(-1);
-    Node prev = dummy;
-    while (n-- > 0) {
-      Node node = new Node(scn.nextInt());
-      prev.right = node;
-      node.left = prev;
-      prev = prev.right;
+        Node(int val) {
+            this.val = val;
+        }
     }
 
-    Node head = dummy.right;
-    head.left = dummy.right = null;
+    public static Node getMidNode(Node node) {
 
-    return head;
-  }
+        if (node == null || node.right == null)
+            return node;
+        Node fast = node, slow = node;
 
-  public static void main(String[] args) {
-    Node head = makeList(scn.nextInt());
+        while (fast.right != null && fast.right.right != null) {
+            fast = fast.right.right;
+            slow = slow.right;
+        }
 
-    head = SortedDLLToBST(head);
-    display(head);
-  }
+        return slow;
+
+    }
+
+    // left : prev, right : next
+    public static Node SortedDLLToBST(Node head) {
+        if (head == null || head.right == null)
+            return head;
+        Node midNode = getMidNode(head);
+        Node root = midNode; 
+        
+        Node nhead = midNode.right;
+        Node prev = midNode.left;
+
+        midNode.left = midNode.right = nhead.left = null;
+        if(prev != null) prev.right = null;
+
+        Node leftNode = prev != null ? head : null;
+        Node rightNode = nhead;
+
+        root.left = SortedDLLToBST(leftNode);
+        root.right = SortedDLLToBST(rightNode);
+        return root;
+    }
+
+    // Input_code===================================================
+
+    public static void display(Node node) {
+        if (node == null)
+            return;
+
+        StringBuilder sb = new StringBuilder();
+        sb.append((node.left != null ? node.left.val : "."));
+        sb.append(" -> " + node.val + " <- ");
+        sb.append((node.right != null ? node.right.val : "."));
+
+        System.out.println(sb.toString());
+
+        display(node.left);
+        display(node.right);
+
+    }
+
+    public static Node makeList(int n) {
+        Node dummy = new Node(-1);
+        Node prev = dummy;
+        while (n-- > 0) {
+            Node node = new Node(scn.nextInt());
+            prev.right = node;
+            node.left = prev;
+            prev = prev.right;
+        }
+
+        Node head = dummy.right;
+        head.left = dummy.right = null;
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+        Node head = makeList(scn.nextInt());
+
+        head = SortedDLLToBST(head);
+        display(head);
+    }
 }
