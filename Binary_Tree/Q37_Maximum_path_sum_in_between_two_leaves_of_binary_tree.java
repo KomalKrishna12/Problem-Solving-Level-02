6bb8cb3475ade597814f29dc594a4f4b9cd304d6
KomalKrishna12 <komalkrishna016@gmail.com>
2022-01-13 12:52:08 +0530

    max path sum between two leaves

--- Binary_Tree/Q37_Maximum_path_sum_in_between_two_leaves_of_binary_tree.java
+++ Binary_Tree/Q37_Maximum_path_sum_in_between_two_leaves_of_binary_tree.java
@@ -17,6 +17,23 @@ public class Q37_Maximum_path_sum_in_between_two_leaves_of_binary_tree{
         int NTLmax = - (int)1e9 - 1; // node to leave max sum
     }
 
+    // we're required to calculate max path sum between two leaves
+    // so there are three possibilities
+    // first  : max sum exists on left subtree
+    // second : max sum exists on right subtree
+    // third  : in left there is one max root to leave path and in right another path
+    //          and merging these two so actual max will be (left max path + right max path + root.val)
+    // so we've manage two varaibles one is leave to leave max sum (LTLmax) and another is root to leave
+    // max sum (NTLmax)
+    // if root is null then no max exists
+    // if root is leave then node to leave path will be root.val but there will be no leave to leave path
+    // suppose left subtree LTLmax = 10 and NTLmax = 5, right subtre LTLmax = 20 and NTLmax = 15, curr = 12
+    // so for current LTLmax will be 
+    // Max of (max of(left LTLmax, right LTLmax),left NTLmax + curr.val + right NTLmax) 
+    // Max of (max of (10, 20), 5 + 12 + 15)
+    // Max of (20, 32) = 32 so here max sum will be left node to leave path + curr.val + right node to leave path
+    // so we're using this approach to find the max path sum between two leave
+    // for LTLmax and NTLmax we have created a class which contain these two variables
     public static Pair maxPathSum_(TreeNode root) {
 
         Pair myAns = new Pair();
@@ -31,10 +48,13 @@ public class Q37_Maximum_path_sum_in_between_two_leaves_of_binary_tree{
         Pair right = maxPathSum_(root.right);
 
         myAns.LTLmax = Math.max(left.LTLmax, right.LTLmax);
+        // here we check the condition that left and right exists or not
+        // if right not exists then no need to check LTLmax for right
         if(root.left != null && root.right != null){
             myAns.LTLmax = Math.max(myAns.LTLmax, left.NTLmax + root.val + right.NTLmax);
         }
 
+        // and here we're also storing NTLmax for root
         myAns.NTLmax = Math.max(left.NTLmax, right.NTLmax) + root.val;
         return myAns;
         
