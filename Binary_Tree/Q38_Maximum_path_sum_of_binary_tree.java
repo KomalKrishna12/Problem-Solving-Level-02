06a3efef45e61beff3bbe58715acc37da580578c
KomalKrishna12 <komalkrishna016@gmail.com>
2022-01-14 23:23:07 +0530

    max path sum

--- Binary_Tree/Q38_Maximum_path_sum_of_binary_tree.java
+++ Binary_Tree/Q38_Maximum_path_sum_of_binary_tree.java
@@ -12,11 +12,16 @@ public class Q38_Maximum_path_sum_of_binary_tree{
     }
   }
 
+  // this is static class Pair in which we're using two variables NTN_maxsum(node to node max sum)
+  // and RTN_maxsum(root to node max sum)
   static class Pair{
     int NTN_maxSum = Integer.MIN_VALUE;
     int RTN_maxSum = 0;
   }
 
+  // this int... arr denotes an array like main(String... s)
+  // so create a max varaible assign arr[0] as max
+  // now using for each loop find max and return it
   public static int getMax(int... arr){
     int max = arr[0];
     for (int val : arr) {
@@ -25,6 +30,14 @@ public class Q38_Maximum_path_sum_of_binary_tree{
     return max;
   }
 
+  // now create a method maxPathsum_(root) which return an object of Pair class
+  // if root is null return pair object which stores the default value
+  // now call for it's left and right
+  // now craete myans object
+  // for RTN compare left pair RTN and right RTN and add root.val in max of both 
+  // now for NTN comapre left NTN, right NTN, root.val, left RTN + right RTN + root.val and RTN which we calculate
+  // just before
+  // now for RTN compare it with root.val
   public static Pair maxPathSum_(TreeNode root) {
     if(root == null) return new Pair();
 
