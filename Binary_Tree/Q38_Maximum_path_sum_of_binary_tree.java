134b70e00e2e49d02c1415a3f4cbbab7bad52958
KomalKrishna12 <komalkrishna016@gmail.com>
2022-01-14 10:09:10 +0530

    maximum path sum

---
+++ Binary_Tree/Q38_Maximum_path_sum_of_binary_tree.java
@@ -0,0 +1,48 @@
+import java.util.*;
+public class Q38_Maximum_path_sum_of_binary_tree{
+    public static Scanner scn = new Scanner(System.in);
+
+  public static class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    TreeNode(int val) {
+      this.val = val;
+    }
+  }
+
+  public static int maxPathSum(TreeNode root) {
+    return 0;
+  }
+
+  // input_Section=================================================
+
+  public static TreeNode createTree(int[] arr, int[] IDX) {
+    if (IDX[0] > arr.length || arr[IDX[0]] == -1) {
+      IDX[0]++;
+      return null;
+    }
+
+    TreeNode node = new TreeNode(arr[IDX[0]++]);
+    node.left = createTree(arr, IDX);
+    node.right = createTree(arr, IDX);
+
+    return node;
+  }
+
+  public static void solve() {
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++)
+      arr[i] = scn.nextInt();
+
+    int[] IDX = new int[1];
+    TreeNode root = createTree(arr, IDX);
+    System.out.println(maxPathSum(root));
+  }
+
+  public static void main(String[] args) {
+    solve();
+  }
+}
