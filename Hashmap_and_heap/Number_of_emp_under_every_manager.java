c2a1b65fd5edc985451d5a78714b276976f415af
KomalKrishna12 <komalkrishna016@gmail.com>
2022-01-25 13:34:54 +0530

    number of emp under every manager

--- Hashmap_and_heap/Number_of_emp_under_every_manager.java
+++ Hashmap_and_heap/Number_of_emp_under_every_manager.java
@@ -7,6 +7,11 @@ public class Number_of_emp_under_every_manager {
         String ceo = "";
         //ceo is the person whose manager is the same person like m is manager of m so m is the ceo
 
+        // so we are creating a loop into map and storing manager into man if man and emp is same
+        // then store it into ceo
+        // else check if the man is in the tree or not
+        // if true then add one more emp to hashset
+        // else create a hashset and add emp into that and put it into tree hashmap 
         for(String emp : map.keySet()){
             String man = map.get(emp);
 
@@ -25,6 +30,8 @@ public class Number_of_emp_under_every_manager {
                 }
             }
         }
+
+        // now create a hashmap result and store manager with it's no of emp
         HashMap<String, Integer> result = new HashMap<>();
         getSize(tree, ceo, result);
         for(String emp : result.keySet()){
@@ -32,6 +39,12 @@ public class Number_of_emp_under_every_manager {
         }
     }
 
+    // now it's a generic tree and we want to calculate size of tree
+    // create a size variable initialize it with 0
+    // now if tree doesn't contains manager then put 0 in no of emp and return 1 (base case)
+    // now create a for each loop for calculating size for every children
+    // add that children size(cs) into size
+    // after loop ended add it into result hashmap and return size + 1
     public static int getSize(HashMap<String, HashSet<String>> tree, String man, HashMap<String, Integer> result){
         int size = 0;
         if(tree.containsKey(man) == false){
