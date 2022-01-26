69f6b4c94b796e197bb25105d7f6205eae528f17
KomalKrishna12 <komalkrishna016@gmail.com>
2022-01-26 11:27:23 +0530

    '''

---
+++ Hashmap_and_heap/Doremon_gadgets.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Doremon_gadgets{
+    public static ArrayList<Integer> TopK(ArrayList<Integer> array, int k)
+    {
+        // code here
+        ArrayList<Integer> ans = new ArrayList<>();
+        return ans;
+    }
+    public static void main(String[] args) {
+        // {1, 1, 1, 2, 2, 3}
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(1);
+        list.add(1);
+        list.add(2);
+        list.add(2);
+        list.add(3);
+        System.out.println(list);
+    }
+}
