51000f4c7375573f5e08e91687e089b45746bf6c
KomalKrishna12 <komalkrishna016@gmail.com>
2022-02-02 23:38:30 +0530

    longest substring of given string

---
+++ Hashmap_and_heap/Q10_longest_substring_of_the_given_string_that_contains_all_non_repeating_characters.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Q10_longest_substring_of_the_given_string_that_contains_all_non_repeating_characters {
+    public static int solution(String str) {
+		// write your code here
+		
+		return 0;
+	}
+	
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String str = scn.next();
+        scn.close();
+		System.out.println(solution(str));
+	}
+}
