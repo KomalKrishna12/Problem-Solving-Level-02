ba3ccf360ff50defa67a298ac970c1c9561e3930
KomalKrishna12 <komalkrishna016@gmail.com>
2022-02-17 19:17:00 +0530

    count of substrings having all unique characters

--- Hashmap_and_heap/Q11_Count_of_substrings_having_all_unique_characters.java
+++ Hashmap_and_heap/Q11_Count_of_substrings_having_all_unique_characters.java
@@ -1,10 +1,37 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Q11_Count_of_substrings_having_all_unique_characters{
     public static int solution(String str) {
-		// write your code here
+        int ans = 0;
+
+        int i = -1, j = -1;
+        HashMap<Character, Integer> map = new HashMap<>();
+        while(true){
+            boolean f1 = false, f2 = false;
+            while(i < str.length() - 1){
+                f1 = true;
+                i++;
+                char ch = str.charAt(i);
+                map.put(ch, map.getOrDefault(ch, 0) + 1);
+                if(map.get(ch) == 2) break;
+                else{
+                    ans += i - j;
+                }
+            }
+            while(j < i){
+                f2 = true;
+                j++;
+                char ch = str.charAt(j);
+                map.put(ch, map.get(ch) - 1);
+                if(map.get(ch) == 1){
+                    ans += i - j;
+                    break;
+                }
+            }
+            if(f1 == false && f2 == false) break;
+        }
 		
-		return 0;
+		return ans;
 	}
 	
 	public static void main(String[] args) {
