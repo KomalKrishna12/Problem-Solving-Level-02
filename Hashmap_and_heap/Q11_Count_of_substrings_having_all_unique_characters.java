700390975707553400c685a87ab280aff9f20190
KomalKrishna12 <komalkrishna016@gmail.com>
2022-02-17 18:23:29 +0530

    count of substrings having all unique characters

--- Hashmap_and_heap/Q11_Count_of_substrings_having_all_unique_characters.java
+++ Hashmap_and_heap/Q11_Count_of_substrings_having_all_unique_characters.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Q11_Count_of_substrings_having_all_unique_characters{
-    public static void main(String[] args) {
-        
-    }
+    public static int solution(String str) {
+		// write your code here
+		
+		return 0;
+	}
+	
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String str = scn.next();
+        scn.close();
+		System.out.println(solution(str));
+	}
 }
