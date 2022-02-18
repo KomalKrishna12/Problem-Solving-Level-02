65cd6674e4ce851ec64f730334fff52dfa5b0b62
KomalKrishna12 <komalkrishna016@gmail.com>
2022-02-18 10:49:58 +0530

    longest_substring_with_exactly_k_unique_characters

---
+++ Hashmap_and_heap/Q12_Longest_substring_with_exactly_k_unique_characters.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Q12_Longest_substring_with_exactly_k_unique_characters{
+    public static int solution(String str, int k){
+		return 0;
+	}
+	
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+        String str = scn.next();
+        int k = scn.nextInt();
+        scn.close();
+		System.out.println(solution(str,k));
+	}
+}
