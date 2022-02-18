071d9cb2c1df8d9a5f4d0292352ded4c8d104744
KomalKrishna12 <komalkrishna016@gmail.com>
2022-02-18 13:13:44 +0530

    longest substring with exactly k unique characters

--- Hashmap_and_heap/Q12_Longest_substring_with_exactly_k_unique_characters.java
+++ Hashmap_and_heap/Q12_Longest_substring_with_exactly_k_unique_characters.java
@@ -1,7 +1,60 @@
 import java.util.*;
 public class Q12_Longest_substring_with_exactly_k_unique_characters{
     public static int solution(String str, int k){
-		return 0;
+        int ans = 0;
+
+		int i = -1, j = -1;
+		HashMap<Character, Integer> map = new HashMap<>();
+
+		while(true){
+			boolean f1 = false, f2 = false;
+
+			while(i < str.length() - 1){
+				f1 = true;
+				i++;
+				char ch = str.charAt(i);
+				map.put(ch, map.getOrDefault(ch, 0) + 1);
+
+				if(map.size() > k){
+					continue;
+				}
+				else if(map.size() == k){
+					int len = i - j;
+					if(len > ans){
+						ans = len;
+					}
+				}
+				else{
+					break;
+				}
+			}
+
+			while(j < i){
+				f2 = true;
+				j++;
+				char ch = str.charAt(j);
+				if(map.get(ch) == 1){
+					map.remove(ch);
+				}
+				else{
+					map.put(ch, map.get(ch) - 1);
+				}
+				if(map.size() > k){
+					continue;
+				}
+				else if(map.size() == k){
+					int len = i - j;
+					if(ans < len){
+						len = ans;
+					}
+					break;
+				}
+			}
+
+			if(f1 == false && f2 == false) break;
+		}
+        
+		return ans;
 	}
 	
 	public static void main(String[] args) {
