992f6e114ea2c6cd43785fbc20b5c4f1e75e3036
KomalKrishna12 <komalkrishna016@gmail.com>
2022-02-22 22:49:01 +0530

    count of substring with exactly k unique characters

---
+++ Hashmap_and_heap/Q13_Count_of_substring_with_exactly_k_unique_characters.java
@@ -0,0 +1,14 @@
+public class Q13_Count_of_substring_with_exactly_k_unique_characters {
+    public static int solution(String str, int k){
+		// write your code here
+
+		return 0;
+	}
+	
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+        String str = scn.next();
+        int k = scn.nextInt();
+		System.out.println(solution(str,k));
+	}
+}
