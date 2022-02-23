ddedfda9222821a57146364d9502c54c5de7ce99
KomalKrishna12 <komalkrishna016@gmail.com>
2022-02-23 23:37:10 +0530

    maximum consecutive ones with one zero flip allowed

---
+++ Hashmap_and_heap/Q15_maximum_consecutive_onex_1.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Q15_maximum_consecutive_onex_1 {
+    public static int solution(int[] arr){
+        int ans = 0;
+        int j = -1, count = 0;
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == 0) count++;
+            while(count > 1){
+                j++;
+                if(arr[j] == 0) count--;
+            }
+            int len = i - j;
+            if(len > ans){
+                ans = len;
+            }
+        }
+
+        return ans;
+    }
+    
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0 ; i  < n; i++){
+            arr[i] = scn.nextInt();
+        }
+        scn.close();
+        System.out.println(solution(arr));
+	}
+}
