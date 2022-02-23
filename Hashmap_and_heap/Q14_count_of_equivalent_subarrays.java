9776a2ede04dbdd31437aafbf56e54afa37f41ae
KomalKrishna12 <komalkrishna016@gmail.com>
2022-02-23 20:48:58 +0530

    count of equivalent subarrays

---
+++ Hashmap_and_heap/Q14_count_of_equivalent_subarrays.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Q14_count_of_equivalent_subarrays{
+    public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0 ; i  < n; i++){
+            arr[i] = scn.nextInt();
+        }
+        scn.close();
+		
+	}
+}
