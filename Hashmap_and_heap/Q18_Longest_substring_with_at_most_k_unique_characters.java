db163e94eb0a0bac4d32aae940dbd55b00f22ad1
KomalKrishna12 <komalkrishna016@gmail.com>
2022-02-24 11:40:39 +0530

    longest substring with atmost k unique characters

--- Hashmap_and_heap/Q18_Longest_substring_with_at_most_k_unique_characters.java
+++ Hashmap_and_heap/Q18_Longest_substring_with_at_most_k_unique_characters.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class Q18_Longest_substring_with_at_most_k_unique_characters {
-    
+    public static int solution(String str, int k) {
+		// write your code here
+		
+		return 0;
+	}
+	
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+        String str = scn.next();
+        int k = scn.nextInt();
+        scn.close();
+		System.out.println(solution(str,k));
+	}
 }
