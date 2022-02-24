01f240ef127fa6f9c2d41b630e6149f195c87b8a
KomalKrishna12 <komalkrishna016@gmail.com>
2022-02-24 13:41:18 +0530

    count of substring with atmost k unique characters

--- Hashmap_and_heap/Q19_count_of_longest_substring_with_atmost_k_unique_characters.java
+++ Hashmap_and_heap/Q19_count_of_longest_substring_with_atmost_k_unique_characters.java
@@ -1,10 +1,47 @@
 import java.util.*;
+// this is same question like Q18 but here we want to find out tottal count of longest sustring 
+// with atmost k unique characters
 public class Q19_count_of_longest_substring_with_atmost_k_unique_characters {
     
     public static int solution(String str, int k) {
-		// write your code here
+		int ans = 0;
 		
-		return 0;
+        int i = -1, j = -1;
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        while(true){
+
+            while(i < str.length() - 1){
+                i++;
+                char ch = str.charAt(i);
+                map.put(ch, map.getOrDefault(ch, 0) + 1);
+                if(map.size() <= k){
+                    ans += i - j;
+                }
+                else{
+                    break;
+                }
+            }
+
+            // this is the condition where we are at last character of string and map size is less
+            // than equal to k so we can break from loop
+            if(i == str.length() - 1 && map.size() <= k) break;
+
+            while(j < i){
+                j++;
+                char ch = str.charAt(j);
+                if(map.get(ch) == 1) map.remove(ch);
+                else map.put(ch, map.get(ch) - 1);
+                if(map.size() > k) continue;
+                else{
+                    ans += i - j;
+                    break;
+                }
+            }
+
+        }
+
+		return ans;
 	}
 	
 	public static void main(String[] args) {
