f649364d24120c71449f37dc8db3eea58b57b855
KomalKrishna12 <komalkrishna016@gmail.com>
2022-02-24 17:13:03 +0530

    find all anagrams in string

---
+++ Hashmap_and_heap/Q20_Find_all_anagrams_in_a_string.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Q20_Find_all_anagrams_in_a_string {
+    public static void findAnagrams(String s, String p) {
+        // write your code here
+        
+	}
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String s = scn.next();
+		String p = scn.next();
+        scn.close();
+		findAnagrams(s, p);
+	}
+}
