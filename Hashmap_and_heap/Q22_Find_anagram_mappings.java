f365c5258add7a94444fdb508312f6b3e2ad388f
KomalKrishna12 <komalkrishna016@gmail.com>
2022-02-24 23:02:46 +0530

    find anagram mappings

--- Hashmap_and_heap/Q22_Find_anagram_mappings.java
+++ Hashmap_and_heap/Q22_Find_anagram_mappings.java
@@ -1,9 +1,42 @@
 import java.util.*;
+// in the question we have given two string both are anagram of each other
+// we are required to map them
+// map means element of arr1 is present at ith index of arr2 so ith index is the mapping of element of
+// arr1
+// {arr1[i] ka value arr2[i] mai dhundna hai or uska index store krna hai ans[i] mai}
 public class Q22_Find_anagram_mappings {
+    static class Pair{
+        int index = 0;
+        ArrayList<Integer> list = new ArrayList<>();
+    }
     public static int[] anagramMappings(int[] arr1, int[] arr2) {
-		// write your code here
+        // create a hashmap of integer and class pair type
+        // traverse in arr2
+        // if map contains arr2[i] then add the index into the list of arr2[i]
+        // else create a pair, add index to its list and put it into map
+        HashMap<Integer, Pair> map = new HashMap<>();
+        for(int i = 0; i < arr2.length; i++){
+            if(map.containsKey(arr2[i])){
+                Pair p = map.get(arr2[i]);
+                p.list.add(i);
+            }
+            else{
+                Pair p = new Pair();
+                p.list.add(i);
+                map.put(arr2[i], p);
+            }
+        }
+        // create a ans array which will store the maped index with respect to arr1
+		int[] ans = new int[arr1.length];
 
-		return null;
+        // get the index and list for arr1[i] and store the index of particular element into ans array
+        for (int i = 0; i < arr1.length; i++) {
+            Pair p = map.get(arr1[i]);
+            ans[i] = p.list.get(p.index);
+            p.index++;
+        }
+
+        return ans;
 	}
 
 	public static void main(String[] args) {
