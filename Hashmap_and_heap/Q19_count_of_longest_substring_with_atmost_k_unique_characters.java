076289d6eadec28fb594b2e72bf5cfb988d8ac60
KomalKrishna12 <komalkrishna016@gmail.com>
2022-02-24 12:48:23 +0530

    count of substring with atmost k unique characters

---
+++ Hashmap_and_heap/Q19_count_of_longest_substring_with_atmost_k_unique_characters.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Q19_count_of_longest_substring_with_atmost_k_unique_characters {
+    
+    public static int solution(String str, int k) {
+		// write your code here
+		
+		return 0;
+	}
+	
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+        String str = scn.next();
+        int k = scn.nextInt();
+        scn.close();
+		System.out.println(solution(str,k));
+	}
+
+}
