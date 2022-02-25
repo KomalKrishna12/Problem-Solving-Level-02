ffed222489853ae3b9cfba37b56ac276abce5c14
KomalKrishna12 <komalkrishna016@gmail.com>
2022-02-25 16:08:02 +0530

    word pattern

--- Hashmap_and_heap/Q26_Word_pattern.java
+++ Hashmap_and_heap/Q26_Word_pattern.java
@@ -1,9 +1,30 @@
 import java.util.*;
+// this is similar question like Q25 isomorphic string
+// but here we've given a two string one is pattern and other str which a sentenece
+// we've to split str with space(" ")
 public class Q26_Word_pattern {
     public static boolean wordPattern(String pattern, String str) {
-		// write your code here
+		String[] words = str.split(" ");
+
+        HashMap<Character, String> map1 = new HashMap<>();
+        HashMap<String, Boolean> map2 = new HashMap<>();
+
+        for(int i = 0; i < pattern.length(); i++){
+            char ch = pattern.charAt(i);
+            if(map1.containsKey(ch)){
+                String s = map1.get(ch);
+                if(s.equals(words[i]) == false) return false;
+            }
+            else{
+                if(map2.containsKey(words[i])) return false;
+                else{
+                    map1.put(ch, words[i]);
+                    map2.put(words[i], true);
+                }
+            }
+        }
 		
-		return false;
+		return true;
 	}
 	
 	public static void main(String[] args) {
