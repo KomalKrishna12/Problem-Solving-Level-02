c0e1591f93e7257eb78b03f8125b008a0b8c5d56
KomalKrishna12 <komalkrishna016@gmail.com>
2022-02-25 14:15:46 +0530

    isomorphic strings

---
+++ Hashmap_and_heap/Q25_Isomorphic_string.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Q25_Isomorphic_string {
+    public static boolean isIsomorphic(String s, String t) {
+		// write your code here
+
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String A = scn.next();
+		String B = scn.next();
+        scn.close();
+		System.out.print(isIsomorphic(A, B));
+	}
+}
