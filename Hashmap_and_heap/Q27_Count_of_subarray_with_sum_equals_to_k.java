537a15b2a88de6f2ec3c2078ef7aca18bb02f411
KomalKrishna12 <komalkrishna016@gmail.com>
2022-02-26 00:09:33 +0530

    count of subarray with sum equal to k

--- Hashmap_and_heap/Q27_Count_of_subarray_with_sum_equals_to_k.java
+++ Hashmap_and_heap/Q27_Count_of_subarray_with_sum_equals_to_k.java
@@ -1,5 +1,14 @@
 import java.util.*;
+// in this question we've given an array and target
+// we're required to count total no of subarrays with sum equals to target
 public class Q27_Count_of_subarray_with_sum_equals_to_k {
+    // for this we create a hashmap and add 0 with freq 1
+    // now start loop
+    // create a sum variable
+    // for every element add sum with arr[i] 
+    // now check the map contains sum-target or not if yes that means their is a subarray with sum target
+    // add the occurence into ans
+    // put the sum by increasing its occurence by 1
     public static int solution(int[] arr, int target){
 		int ans = 0;
 
