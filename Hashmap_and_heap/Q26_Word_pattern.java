c2e1f2c392cbe520ceb5898600bfce4737fd2efb
KomalKrishna12 <komalkrishna016@gmail.com>
2022-02-25 15:17:05 +0530

    word pattern

---
+++ Hashmap_and_heap/Q26_Word_pattern.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Q26_Word_pattern {
+    public static boolean wordPattern(String pattern, String str) {
+		// write your code here
+		
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String pattern = scn.nextLine();
+		String words = scn.nextLine();
+        scn.close();
+		System.out.println(wordPattern(pattern,words));
+	}
+}
