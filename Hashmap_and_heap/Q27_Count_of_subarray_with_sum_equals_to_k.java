9344c5a8e7cf0bf0089787725d66b9583fa3625c
KomalKrishna12 <komalkrishna016@gmail.com>
2022-02-25 23:55:12 +0530

    count of subarray with sum equal to k

--- Hashmap_and_heap/Q27_Count_of_subarray_with_sum_equals_to_k.java
+++ Hashmap_and_heap/Q27_Count_of_subarray_with_sum_equals_to_k.java
@@ -1,9 +1,21 @@
 import java.util.*;
 public class Q27_Count_of_subarray_with_sum_equals_to_k {
     public static int solution(int[] arr, int target){
-		// write your code here
+		int ans = 0;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int sum = 0;
+
+        for(int i = 0; i < arr.length; i++){
+            sum += arr[i];
+            if(map.containsKey(sum - target)){
+                ans += map.get(sum - target);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
 		
-		return 0;
+		return ans;
 	}
 	
 	public static void main(String[] args) {
