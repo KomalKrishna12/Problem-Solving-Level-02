a63efaf0b166f62af1a57560ebd9137027095f2d
KomalKrishna12 <komalkrishna016@gmail.com>
2022-02-26 15:16:57 +0530

    longest subarray with sum divisible by k

--- Hashmap_and_heap/Q28_Longest_subarray_with_sum_divisible_by_k.java
+++ Hashmap_and_heap/Q28_Longest_subarray_with_sum_divisible_by_k.java
@@ -1,9 +1,40 @@
 import java.util.*;
+// in this qustion an array and a variable k is given
+// we're required to find out the len of longest subarray whose sum can be divisble by k
 public class Q28_Longest_subarray_with_sum_divisible_by_k {
     public static int solution(int[] arr, int k) {
-        // write your code here
+        // create a hashmap and in that map store rem and index
+        // for the first time store rem as -1 and index as -1
+        // create two variable sum and rem, in sum add arr[i] with prev value and in rem calculate rem
+        // by dividing sum by k and if rem becomes negetive add k to make it positive
+        // now check the map contains rem or not, if yes then find out its index and calculate len and
+        // update if len is gretaer than prev
+        // else add rem into the map with index i 
+        int ans = 0;
 
-        return 0;
+        int sum = 0, rem = 0;
+        HashMap<Integer, Integer> map = new HashMap<>(); // remainder(key), index(value)
+        map.put(0, -1);
+
+        for(int i = 0; i < arr.length; i++){
+            sum += arr[i];
+            rem = sum % k;
+
+            if(rem < 0) rem += k;
+            if(map.containsKey(rem)){
+                int idx = map.get(rem);
+                int len = i - idx;
+                if(len > ans){
+                    ans = len;
+                }
+            }
+            else{
+                map.put(rem, i);
+            }
+        }
+
+        return ans;
+    
     }
     
     public static void main(String[] args) {
