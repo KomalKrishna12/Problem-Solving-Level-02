8e785a0ab42985aaa26ad5671ae5981d7ffeb741
KomalKrishna12 <komalkrishna016@gmail.com>
2022-02-26 18:40:55 +0530

    count of subarray with sum divisble by k

--- Hashmap_and_heap/Q29_count_of_Subarrays_with_sum_divisible_by_k.java
+++ Hashmap_and_heap/Q29_count_of_Subarrays_with_sum_divisible_by_k.java
@@ -1,7 +1,18 @@
 import java.util.*;
+// this is similar question like Q27 but here we're required to find out subarray whose sum divisble by k
+// not equal to k
 public class Q29_count_of_Subarrays_with_sum_divisible_by_k{
     public static int solution(int[] arr, int k) {
         int ans = 0;
+
+        // we use same conecpt 
+        // create a hasmap for remainder and it's freq
+        // create a variable sum and rem
+        // add first element as 0 remainder and its freq as 1
+        // now traverse into the loop
+        // add arr[i] into sum
+        // now find out remainder and check it's existing into the map or not 
+        // if yes then find its freq and add it to ans and increase its freq into map
 
         HashMap<Integer, Integer> map = new HashMap<>();
         map.put(0, 1);
