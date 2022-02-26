3f752b35221734bdd765aca14affe816fbb2586d
KomalKrishna12 <komalkrishna016@gmail.com>
2022-02-26 19:33:54 +0530

    count of subarray with equal no of 0's and 1's

--- Hashmap_and_heap/Q31_Counnt_of_subarray_with_equal_no_of_0s_1s.java
+++ Hashmap_and_heap/Q31_Counnt_of_subarray_with_equal_no_of_0s_1s.java
@@ -1,8 +1,16 @@
 import java.util.*;
+// in this ques we've given an array with elements 0's and 1's
+// we're required to find out total no of subarrays with equal no of 0's and 1's
 public class Q31_Counnt_of_subarray_with_equal_no_of_0s_1s {
     public static int solution(int[] arr) {
         int ans = 0;
 
+        // create a hashmap for key as sum and value as freq
+        // add first elements as sum 0 and freq 1
+        // now traverse into array arr
+        // if arr[i] is 0 then add -1 into sum var else add -1 for 1
+        // if map contains sum then add it's value into ans and increase its freq by 1
+        // else add sum with freq 1 
         HashMap<Integer, Integer> map = new HashMap<>();
         map.put(0, 1);
         int sum = 0;
