6590ef888dc2d38d35bbe870737b016c2176fa67
KomalKrishna12 <komalkrishna016@gmail.com>
2022-02-26 22:44:30 +0530

    longest subarray with equal no of 0's 1's and 2's

--- Hashmap_and_heap/Q32_Longest_subarray_with_equal_no_of_0s_1s_2s.java
+++ Hashmap_and_heap/Q32_Longest_subarray_with_equal_no_of_0s_1s_2s.java
@@ -1,9 +1,38 @@
 import java.util.*;
 public class Q32_Longest_subarray_with_equal_no_of_0s_1s_2s {
     public static int solution(int[] arr) {
-        // write your code here
+        int ans = 0;
 
-        return 0;
+        int countz = 0; // count of zero's
+        int counto = 0; // count of one's
+        int countt = 0; // count of two's
+        int delta10 = counto - countz;
+        int delta21 = countt - counto;
+        String key = delta10 + "#" + delta21;
+
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put(key, -1);
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == 0) countz++;
+            else if(arr[i] == 1) counto++;
+            else countt++;
+            delta10 = counto - countz;
+            delta21 = countt - counto;
+            key = delta10 + "#" + delta21;
+
+            if(map.containsKey(key)){
+                int idx = map.get(key);
+                int len = i - idx;
+                if(len > ans) ans = len;
+            }
+            else{
+                map.put(key, i);
+            }
+        }
+
+
+        return ans;
     }
     
     public static void main(String[] args) {
