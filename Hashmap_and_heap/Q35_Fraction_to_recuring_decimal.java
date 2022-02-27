b78ea31f531603db0e71669361be5c3b61463b7d
KomalKrishna12 <komalkrishna016@gmail.com>
2022-02-27 15:03:20 +0530

    fraction to recuring decimal

---
+++ Hashmap_and_heap/Q35_Fraction_to_recuring_decimal.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Q35_Fraction_to_recuring_decimal {
+    public static String solution(int num, int den) {
+        // write your code here
+        
+        return null;
+    }
+    
+    public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int num = scn.nextInt();
+		int den = scn.nextInt();
+        scn.close();
+		System.out.println(solution(num , den));
+	}
+}
