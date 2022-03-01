c63195a415eb2fee8b61fa9db4bcfc4028e0f18a
KomalKrishna12 <komalkrishna016@gmail.com>
2022-03-01 12:32:43 +0530

    quadruple sum2

--- Hashmap_and_heap/Q43_Quadruple_sum2.java
+++ Hashmap_and_heap/Q43_Quadruple_sum2.java
@@ -1,10 +1,30 @@
+import java.util.HashMap;
 import java.util.Scanner;
-
+// in this ques we have given four arrays 
+// we are required to find out the total count of elements which can make A[a] + B[b] + C[c] + D[d] = 0;
 public class Q43_Quadruple_sum2 {
     public static int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
-		// write your code here
+		// create a hashmap and store e1(elemet of array A) + e2(element of array B) into the map
+		// check target - [e1(element of array C) + e2(element of array D)] into the map
+		// if it is available then increase the count
+		HashMap<Integer, Integer> map = new HashMap<>();
+		for(int e1 : A){
+			for(int e2 : B){
+				map.put(e1 + e2, map.getOrDefault(e1 + e2, 0) + 1);
+			}
+		}
 
-		return 0;
+		int count = 0;
+		int target = 0; // given in question 
+
+		for(int e1 : C){
+			for(int e2 : D){
+				count += map.getOrDefault(target - (e1 + e2), 0);
+			}
+		}
+
+
+		return count;
 	}
 
 	public static void main(String[] args) {
