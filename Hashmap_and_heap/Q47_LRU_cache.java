7f5ef62650109d334b750fd03432ad5d422e5f95
KomalKrishna12 <komalkrishna016@gmail.com>
2022-03-02 00:23:27 +0530

    LRU cache

--- Hashmap_and_heap/Q47_LRU_cache.java
+++ Hashmap_and_heap/Q47_LRU_cache.java
@@ -1,11 +1,34 @@
 import java.util.*;
 import java.io.*;
+// LRU (least recent used)
+// ex : cap = 3
+// put 1 10 -- add this
+// put 2 20 -- add this
+// put 3 30 -- add this
+// get 1    -- show this value and make it in recent used 
+// put 1 15 -- update its value and make it in recent used
+// get 1    -- show this value and make it in recent used 
+// get 2    -- show this value and make it in recent used 
+// put 4 40 -- cap is already 3 so remove least used and add this
+// get 3    -- show this value and make it in recent used 
+// get 4    -- show this value and make it in recent used 
+// Stop
 public class Q47_LRU_cache {
     public static class LRU {
+        // create a node class for linked list bcoz using linked list we can add in first
+        // remove last and get any node
+        // use doubly linked list bcoz in this we can easly traverse through the list using next and prev
+        // key will be 1 is(1-10), 10 will be value
         class Node{
             int key, value;
             Node next, prev;
         }
+        // we have two node head and prev
+        // craete a nbr node which will store next of head
+        // now next of node will point to nbr
+        // nbr prev point to node
+        // head next point to node 
+        // node prev point to head
         public void addNode(Node node){
             Node nbr = head.next; // nabor of head
             node.next = nbr;
@@ -13,6 +36,10 @@ public class Q47_LRU_cache {
             head.next = node;
             node.prev = head;
         }
+        // to remove craete two node one is prevnbr and another is nextnbr
+        // prevnbr will store prev of node and nextnbr will store next of node
+        // now next of prevnbr point to nextnbr and nextnbr prev point to prevnbr
+        // and next and prev of node poin to null
         public void removeNode(Node node){
             Node prevNbr = node.prev;
             Node nxtNbr = node.next;
@@ -20,13 +47,14 @@ public class Q47_LRU_cache {
             nxtNbr.prev = prevNbr;
             node.next = node.prev = null;
         }
+        // remove that node and add in the first
         public void moveToFirst(Node node){
             removeNode(node);
             addNode(node);
         }
         HashMap<Integer, Node> map;
         Node head, tail;
-        int cap;
+        int cap; // if cap reached then remove least used node and add the new node into recent
         LRU(int capacity) {
             cap = capacity;
             map = new HashMap<>();
@@ -36,6 +64,9 @@ public class Q47_LRU_cache {
             tail.prev = head;
         }
     
+        // create a node and get from the map for the key
+        // if node is null that means that key node is not exist so return -1
+        // else move the node to recent used and return value for that key
         public int get(int key) {
             Node node = map.get(key);
             if(node == null) return -1;
@@ -46,6 +77,13 @@ public class Q47_LRU_cache {
             }
         }
     
+        // for put check wheather node for the key value exists or not
+        // if node null that means not exists
+        // so create newnode, set its key and value
+        // now check the cap
+        // if cap reached then remove last node using tail.prev
+        // now put new node 
+        // if node already exists then update its value and move to recent used
         public void put(int key, int value) {
             Node node = map.get(key);
             if(node == null){
