8289111a3ce284f58f6a76a82b27a2d0fdc02452
KomalKrishna12 <komalkrishna016@gmail.com>
2022-03-03 23:31:37 +0530

    print value of rsb mask

--- Bit_manipulation/Q2_Print_value_of_RSB_mask.java
+++ Bit_manipulation/Q2_Print_value_of_RSB_mask.java
@@ -1,5 +1,13 @@
 import java.util.Scanner;
-
+// RSB means right most set bit mask
+// so in this question we have given a number n
+// we are required to find out its RSB
+// RSB is the msb of binary no, means last 1's, after this in the binary no only 0's are present
+// RSB = n & n"
+// n" : two complement
+// n" = n' + 1;
+// n' : ones complement
+// n" == -n
 public class Q2_Print_value_of_RSB_mask {
     public static void main(String[] args){
         Scanner scn = new Scanner(System.in);
