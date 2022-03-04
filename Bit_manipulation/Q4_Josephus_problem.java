6064bf3d4f9332b12266fefe1ac1eb9bd392a4bd
KomalKrishna12 <komalkrishna016@gmail.com>
2022-03-04 10:41:19 +0530

    josephus problem

--- Bit_manipulation/Q4_Josephus_problem.java
+++ Bit_manipulation/Q4_Josephus_problem.java
@@ -1,9 +1,19 @@
 import java.util.Scanner;
 
 public class Q4_Josephus_problem {
+    public static int powerOf2(int n){
+        int i = 1;
+
+        while (i * 2 <= n) {
+            i = i * 2;
+        }
+
+        return i;
+    }
     public static int solution(int n){
-        //write your code here
-        return 0;
+        int highPowof2 = powerOf2(n);
+        int l = n - highPowof2;
+        return 2 * l + 1;
       }
       public static void main(String[] args){
         Scanner scn = new Scanner(System.in);
