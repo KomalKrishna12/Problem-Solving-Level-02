77da09f5efea503d7321e610141ecc04213151a3
KomalKrishna12 <komalkrishna016@gmail.com>
2022-03-04 11:15:39 +0530

    josephus problem

--- Bit_manipulation/Q4_Josephus_problem.java
+++ Bit_manipulation/Q4_Josephus_problem.java
@@ -1,5 +1,12 @@
 import java.util.Scanner;
-
+// in the ques we have given n solders standing in circular from 1 to n
+// king want to kill every second solders like 1 will safe 2 will killed 3 will be safe 4 will be killed
+// so solders will become less at end when only one solder left that will give freedom any we are rquired
+// to find out that soldrs
+// n = 2^x + l and ans = 2*l + 1
+// break n into pow of 2 and calculate l
+// n = 18
+// n = 2^4 + 2 here l is 2 so ans will be 2*2 + 1 = 5
 public class Q4_Josephus_problem {
     public static int powerOf2(int n){
         int i = 1;
