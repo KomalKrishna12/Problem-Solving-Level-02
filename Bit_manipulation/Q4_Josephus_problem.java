8fb56d2254f69a9a42c6edf0dae2987fc9f3c272
KomalKrishna12 <komalkrishna016@gmail.com>
2022-03-04 10:26:10 +0530

    josephus problem

---
+++ Bit_manipulation/Q4_Josephus_problem.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Q4_Josephus_problem {
+    public static int solution(int n){
+        //write your code here
+        return 0;
+      }
+      public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        scn.close();
+        System.out.println(solution(n));
+      }
+}
