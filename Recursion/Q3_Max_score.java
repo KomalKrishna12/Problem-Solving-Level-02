8b66793b2051bca87f961db4c61c856cc65cbd54
KomalKrishna12 <komalkrishna016@gmail.com>
2022-03-12 16:58:20 +0530

    max score

--- Recursion/Q3_Max_score.java
+++ Recursion/Q3_Max_score.java
@@ -1,9 +1,33 @@
 import java.util.*;
 public class Q3_Max_score {
     public static int solution(String[] words, int[] farr, int[] score, int idx) {
-        //write your code here
+        if(idx == words.length) return 0;
+
+        // if we are not including the word then the score
+        int scoreNo = solution(words, farr, score, idx + 1);
+
+        // if we are including the word then
+        int sword = 0; // score of word
+        boolean flag = true; // this will help us to calculate the score for yes
+        String word = words[idx]; // using this we get our word present at idx
+        for(int i = 0; i < word.length(); i++){
+            char ch = word.charAt(i);
+            if(farr[ch - 'a'] == 0) flag = false; // check that the ch freq is present or not
+            farr[ch - 'a']--; // decrease it's freq and adding the score for the particular char
+            sword += score[ch - 'a'];
+        }
+
+        int scoreYes = 0;
+        // if flag is true then only add the word score with solution()
+        if(flag) scoreYes = sword + solution(words, farr, score, idx + 1);
+
+        // backtracking so we can search for other subset
+        for(int i = 0; i < word.length(); i++){
+            char ch = word.charAt(i);
+            farr[ch - 'a']++;
+        }
     
-        return 0;
+        return Math.max(scoreYes, scoreNo);
       }
     
       public static void main(String[] args) {
