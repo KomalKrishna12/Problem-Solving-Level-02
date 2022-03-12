f3527763db4b18d6e2d6957be1c1267b67eb243b
KomalKrishna12 <komalkrishna016@gmail.com>
2022-03-12 20:25:53 +0530

    lexicographical numbers

---
+++ Recursion/Q5_Lexicographical_numbers.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Q5_Lexicographical_numbers {
+    public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+        scn.close();
+	}
+}
