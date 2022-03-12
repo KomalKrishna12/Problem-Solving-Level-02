7ed7904ad04b939408feec78f3f58b961990bb60
KomalKrishna12 <komalkrishna016@gmail.com>
2022-03-12 19:06:10 +0530

    josephus problem

---
+++ Recursion/Q4_Josephus_problem.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Q4_Josephus_problem {
+    public static int solution(int n, int k){
+        //write your code here
+        return 0;
+      }
+      public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int k = scn.nextInt();
+        scn.close();
+        System.out.println(solution(n,k));
+      }
+}
