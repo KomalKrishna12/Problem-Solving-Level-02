e54c294e3772f672a836180c0f50ed9924cfe9ce
KomalKrishna12 <komalkrishna016@gmail.com>
2022-03-12 23:24:05 +0530

    gold mine 2

--- Recursion/Q6_Gold_mine_2.java
+++ Recursion/Q6_Gold_mine_2.java
@@ -1,8 +1,18 @@
 import java.util.*;
+// in this question we have given a 2d array
+// each row & col contains some amount
+// we are required to collect gold from highest value
+// if value is 0 we can collect
+// if that row & col is already visited then we can not visit again
+// we can move in top, left, righ and down
+// at end print max amount
 public class Q6_Gold_mine_2 {
     public static int getMaximumGold(int[][] grid) {
         int max = 0;
         
+        // trarese in each row & col
+        // check the amount in that particular box if it is not zero then call helper() with grid, row 
+        // col and compaer val with max and update
         for(int i = 0; i < grid.length; i++){
             
             for(int j = 0; j < grid[i].length; j++){
@@ -22,10 +32,21 @@ public class Q6_Gold_mine_2 {
     }
     
     public static int helper(int[][] grid, int i, int j){
-        
+
+        // if row or col is less than zero means beyond lower limit or upper limit then return 0
+        // if box value is 0 then also return 0 
+        // if box value is negetive then also return 0  
+
         if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == 0 ||
           grid[i][j] < 0) return 0;
         
+        // create a variable val to store the particular amount
+        // and mark that row as negetive so we cannot visit it again
+        // now call top, right, left and down
+        // and compare each values and store max into ans variable
+        // now set the box with it's prev value
+        // return ans with the val
+          
         int val = grid[i][j];
         
         grid[i][j] = -val;
