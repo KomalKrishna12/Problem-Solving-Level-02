b249fd21708e9de2845b1daea27b7f0e9ec3f865
KomalKrishna12 <komalkrishna016@gmail.com>
2022-03-15 22:17:06 +0530

    friends pairing 2

--- Recursion/Q10_Friends_pairing2.java
+++ Recursion/Q10_Friends_pairing2.java
@@ -4,9 +4,11 @@ public class Q10_Friends_pairing2 {
     static int counter = 1;
 
     public static void solution(int i, int n, boolean[] used, String asf) {
-        // base case when i is greater than n then print asf with counter bcoz we want to print
+        // base case when i is greater than n then print asf with counter bcoz we want
+        // to print
         // in an order, after print increament the counter value by 1
-        if(n > i){
+        // we are checking for greater not equal bcoz we want to print nth value also
+        if (i > n) {
             System.out.println(counter + "." + asf);
             counter++;
             return;
@@ -14,19 +16,23 @@ public class Q10_Friends_pairing2 {
         if (used[i] == true) // that means this i is already used so just call solution for next level
             solution(i + 1, n, used, asf);
         else {
-            // that mean this i value is not used so firstly mark is as true in used[] then call
-            // solution by adding for single and then multiple pair options which is possible
+            // that mean this i value is not used so firstly mark is as true in used[] then
+            // call
+            // solution by adding for single and then multiple pair options which is
+            // possible
 
             // for single
             used[i] = true;
-            solution(i + 1, n, used, asf + "(" + i + ") "); 
+            solution(i + 1, n, used, asf + "(" + i + ") ");
 
             // for pairs we have multiple options which start from i+1 till n, including n
-            // i+1 bcoz we want unique pairs, so start loop 
-            for(int j = i + 1; j <= n; j++){
-                used[j] = true;
-                solution(i + 1, n, used, asf + "(" + i + j + ") ");
-                used[j] = false;
+            // i+1 bcoz we want unique pairs, so start loop
+            for (int j = i + 1; j <= n; j++) {
+                if (used[j] == false) {
+                    used[j] = true;
+                    solution(i + 1, n, used, asf + "(" + i + "," + j + ") ");
+                    used[j] = false;
+                }
             }
             used[i] = false;
         }
