e114386e14836482009c0b58548add7e00fea01e
KomalKrishna12 <komalkrishna016@gmail.com>
2022-03-16 21:14:58 +0530

    pallindromic permutations

---
+++ Recursion/Q12_All_pallindromic_permutation.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Q12_All_pallindromic_permutation {
+    public static void generatepw(int cs, int ts, HashMap<Character, Integer> fmap, Character oddc, String asf) {
+		
+	}
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String str = scn.next();
+        scn.close();
+		HashMap<Character, Integer> fmap = new HashMap<>();
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			fmap.put(ch, fmap.getOrDefault(ch, 0) + 1);
+		}
+		
+		//write your code here
+	}
+}
