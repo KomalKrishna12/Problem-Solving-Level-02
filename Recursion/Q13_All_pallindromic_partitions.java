a04d676e8a79026ed18c576cd7ef8c00cecbd152
KomalKrishna12 <komalkrishna016@gmail.com>
2022-03-17 08:45:53 +0530

    all pallindromic partitions

---
+++ Recursion/Q13_All_pallindromic_partitions.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Q13_All_pallindromic_partitions {
+    public static void solution(String str, String asf) {
+		//write you code here
+		
+	}
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String str = scn.nextLine();
+        scn.close();
+		solution(str, "");
+	}
+}
