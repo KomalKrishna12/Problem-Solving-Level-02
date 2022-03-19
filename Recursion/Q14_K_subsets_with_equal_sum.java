9d7c442d31bf0b1cdb528a333bb289ef7b7fbc10
KomalKrishna12 <komalkrishna016@gmail.com>
2022-03-19 16:05:26 +0530

    k subsets with equal sum

--- Recursion/Q14_K_subsets_with_equal_sum.java
+++ Recursion/Q14_K_subsets_with_equal_sum.java
@@ -1,7 +1,52 @@
 import java.util.*;
+// this is similar ques like q11_k_subsets but in addition here we want to print all subsets
+// with equal sum
+// suppose k is 3 so all 3 subsets sum have to be equal
 public class Q14_K_subsets_with_equal_sum {
     public static void solution(int[] arr, int vidx,int n , int k,int[] subsetSum,int ssssf, ArrayList<ArrayList<Integer>> ans) {
-		//write your code here
+		if(vidx == arr.length){
+            if(ssssf == k){
+                // check that k subsets are created or not, either any set is empty then return bcoz
+                // it's not a valid ans
+                boolean flag = true;
+                for(int i = 0; i < subsetSum.length - 1; i++){
+                    // now compare sum of all k subsets
+                    // if they are equal then print the ans
+                    if(subsetSum[i] != subsetSum[i + 1]){
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag){
+                    for(ArrayList<Integer> list : ans){
+                        System.out.print(list + " ");
+                    }
+                    System.out.println();
+                }
+            }
+            return;
+        }
+
+        for(int i = 0; i < ans.size(); i++){
+            if(ans.get(i).size() > 0){
+                // here we are adding arr[vidx] element in the existing subsets so sssf will not increase
+                ans.get(i).add(arr[vidx]);
+                subsetSum[i] += arr[vidx]; // add the ith element into subset of vidx element
+                solution(arr, vidx + 1, n, k, subsetSum, ssssf, ans);
+                subsetSum[i] -= arr[vidx];
+                ans.get(i).remove(ans.get(i).size() - 1);
+            }
+            else{
+                // here we are adding into new subset so sssf will increase
+                ans.get(i).add(arr[vidx]);
+                subsetSum[i] += arr[vidx]; // add the ith element into subset of vidx element
+                solution(arr, vidx + 1, n, k, subsetSum, ssssf + 1, ans);
+                subsetSum[i] -= arr[vidx];
+                ans.get(i).remove(ans.get(i).size() - 1);
+                break;
+            }
+        }
+
 	}
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
