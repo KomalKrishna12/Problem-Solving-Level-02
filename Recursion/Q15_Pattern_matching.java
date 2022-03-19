098d30a3da03ef810e496c39c392ac66a132c582
KomalKrishna12 <komalkrishna016@gmail.com>
2022-03-19 17:38:02 +0530

    pattern matching

---
+++ Recursion/Q15_Pattern_matching.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Q15_Pattern_matching {
+    public static void solution(String str, String pattern, HashMap<Character,String> map, String op){
+		//write your code here
+		
+	}
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String str = scn.next();
+		String pattern = scn.next();
+        scn.close();
+		HashMap<Character,String> map = new HashMap<>();
+		solution(str,pattern,map,pattern);
+	}
+}
