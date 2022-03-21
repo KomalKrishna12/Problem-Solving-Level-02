de8c0423f046827cb7fb2d873231bb9a87dc3319
KomalKrishna12 <komalkrishna016@gmail.com>
2022-03-21 15:52:20 +0530

    permutation-I

---
+++ Recursion/Q20_Permutation_i.java
@@ -0,0 +1,29 @@
+import java.io.*;
+// in this question we have give a box of size nboxes and ritems
+// we have to place these items in the boxes
+// we can fill the box with items
+public class Q20_Permutation_i {
+    public static void permutations(int[] boxes, int ci, int ti){
+        if(ci > ti){
+            for(int val : boxes){
+                System.out.print(val);
+            }
+            System.out.println();
+            return;
+        }
+      for(int i = 0; i < boxes.length; i++){
+          if(boxes[i] == 0){
+              boxes[i] = ci;
+              permutations(boxes, ci + 1, ti);
+              boxes[i] = 0;
+          }
+      }
+      }
+    
+      public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int nboxes = Integer.parseInt(br.readLine());
+        int ritems = Integer.parseInt(br.readLine());
+        permutations(new int[nboxes], 1, ritems);
+      }
+}
