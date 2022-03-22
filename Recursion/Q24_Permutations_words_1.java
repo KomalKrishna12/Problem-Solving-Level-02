9568d471df554643a8ee8b94a0c99ac8b389d820
KomalKrishna12 <komalkrishna016@gmail.com>
2022-03-22 17:38:11 +0530

    permutation words 1

--- Recursion/Q24_Permutations_words_1.java
+++ Recursion/Q24_Permutations_words_1.java
@@ -1,9 +1,22 @@
 import java.util.*;
 import java.io.*;
-
+// we have given a word we are required to print all the permutations 
 public class Q24_Permutations_words_1 {
     public static void generateWords(int cs, int ts, HashMap<Character, Integer> fmap, String asf) {
-        // write your code here
+        if(cs > ts){
+            System.out.println(asf);
+            return;
+        }
+
+        // run an loop for fmap and check all char
+        // if their freq is greater than 0 then decrease the freq and call for next char
+        for(char ch : fmap.keySet()){
+            if(fmap.get(ch) > 0){
+                fmap.put(ch, fmap.get(ch) - 1);
+                generateWords(cs + 1, ts, fmap, asf + ch);
+                fmap.put(ch, fmap.get(ch) + 1);
+            }
+        }
       }
     
       public static void main(String[] args) throws Exception {
