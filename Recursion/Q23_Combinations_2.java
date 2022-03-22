610a27fbd7df6449b5bb24e27808d9977bba1592
KomalKrishna12 <komalkrishna016@gmail.com>
2022-03-22 10:40:21 +0530

    combination

--- Recursion/Q23_Combinations_2.java
+++ Recursion/Q23_Combinations_2.java
@@ -4,6 +4,8 @@ public class Q23_Combinations_2 {
   // ci : current item
   // ti : total item
     public static void combinations(boolean[] boxes, int ci, int ti, int llb){
+      // when ci becomes greater than ti then print the boxes
+      // if true print "i" else "-"
         if(ci > ti){
           for(int i = 0; i < boxes.length; i++){
             if(boxes[i]) System.out.print("i");
@@ -13,6 +15,8 @@ public class Q23_Combinations_2 {
           return;
         }
 
+        // we will start placing items from llb + 1
+        // if if boxes is false then mark it true
         for(int b = llb + 1; b < boxes.length; b++){
           if(boxes[b] == false){
             boxes[b] = true;
