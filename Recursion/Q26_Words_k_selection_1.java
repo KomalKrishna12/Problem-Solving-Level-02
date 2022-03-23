9b226b10c724317b0ec8e147976592b2a16cb089
KomalKrishna12 <komalkrishna016@gmail.com>
2022-03-23 22:13:05 +0530

    words k selection 1

--- Recursion/Q26_Words_k_selection_1.java
+++ Recursion/Q26_Words_k_selection_1.java
@@ -1,4 +1,13 @@
 import java.util.*;
+// in this ques we have given a word which may contain characters repeatadly
+// we have given k
+// we're reuired to print all the strings with k uniuqe characters
+
+// this is similar ques like we have n boxes k distinct elements
+// for make our ques like this create a hashset and add all char
+// now pass that char into function
+// first call will be for adding str.CharAt(i)
+// second call for adding nothing
 public class Q26_Words_k_selection_1 {
     public static void main(String[] args) throws Exception {
         Scanner scn = new Scanner(System.in);
@@ -20,6 +29,14 @@ public class Q26_Words_k_selection_1 {
     
     
       public static void combination(int i, String ustr, int ssf, int ts, String asf ) {
-    
+          if(i == ustr.length()){
+              if(ssf == ts) System.out.println(asf);
+              return;
+          }
+
+          char ch = ustr.charAt(i);
+          combination(i + 1, ustr, ssf + 1, ts, asf + ch);
+          combination(i + 1, ustr, ssf, ts, asf + "");
+          
       }
 }
