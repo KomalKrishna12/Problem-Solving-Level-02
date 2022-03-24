80ec222395fbf87b618fe6636cbfc886d22f2d98
KomalKrishna12 <komalkrishna016@gmail.com>
2022-03-24 11:31:11 +0530

    queens combinations

---
+++ Recursion/Q30_Queens_combinations_2d_as_2d_box_chooses.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Q30_Queens_combinations_2d_as_2d_box_chooses {
+    public static void queensCombinations(int qpsf, int tq, int row, int col, String asf) {
+        // write your code here
+      }
+      public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+    
+        queensCombinations(0, n, 0, 0, "");
+      }
+}
