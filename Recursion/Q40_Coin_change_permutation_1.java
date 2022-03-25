aa2b782a2f2aa9d9b6e1f8a40e3cf05739c57549
KomalKrishna12 <komalkrishna016@gmail.com>
2022-03-25 13:55:27 +0530

    coin change permutation 1

---
+++ Recursion/Q40_Coin_change_permutation_1.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Q40_Coin_change_permutation_1 {
+    public static void coinChange(int[] coins, int amtsf, int tamt, String asf, boolean[] used) {
+
+        
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] coins = new int[n];
+        for (int i = 0; i < n; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+        int amt = Integer.parseInt(br.readLine());
+        boolean[] used = new boolean[coins.length];
+        coinChange(coins, 0, amt, "", used);
+    }
+}
