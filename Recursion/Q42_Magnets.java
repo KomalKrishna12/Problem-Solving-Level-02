d66fa9da996bbb284a11c2ebebf9f93735420153
KomalKrishna12 <komalkrishna016@gmail.com>
2022-03-25 22:10:21 +0530

    word k selection

---
+++ Recursion/Q42_Magnets.java
@@ -0,0 +1,49 @@
+import java.util.*;
+public class Q42_Magnets {
+    public static boolean solution(char[][] arr, int[] top, int[] left, int[] right, int[] bottom, char[][] ans,
+                                 int row, int col) {
+
+    //write your code here
+
+    return false;
+  }
+
+  public static void print(char[][] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = 0; j < arr[0].length; j++) {
+        System.out.print(arr[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
+
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    int m = scn.nextInt();
+    int n = scn.nextInt();
+    char[][] arr = new char[m][n];
+    for (int i = 0; i < arr.length; i++) {
+      String str = scn.next();
+      arr[i] = str.toCharArray();
+    }
+    int[] top = new int[n];
+    for (int i = 0; i < n; i++) {
+      top[i] = scn.nextInt();
+    }
+    int[] left = new int[m];
+    for (int i = 0; i < m; i++) {
+      left[i] = scn.nextInt();
+    }
+    int[] right = new int[m];
+    for (int i = 0; i < m; i++) {
+      right[i] = scn.nextInt();
+    }
+    int[] bottom = new int[n];
+    for (int i = 0; i < n; i++) {
+      bottom[i] = scn.nextInt();
+    }
+    scn.close();
+
+    //write your code here
+  }
+}
---
+++ Recursion/Q43_Words_k_selection_3.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+public class Q43_Words_k_selection_3 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int k = Integer.parseInt(br.readLine());
+    
+        HashMap<Character, Integer> unique = new HashMap<>();
+        String ustr = "";
+        for (char ch : str.toCharArray()) {
+          if (unique.containsKey(ch) == false) {
+            unique.put(ch, 1);
+            ustr += ch;
+          } else {
+            unique.put(ch, unique.get(ch) + 1);
+          }
+        }
+    
+       fun(ustr, unique, 0, "", k);
+
+      }
+
+      public static void fun(String str, HashMap<Character, Integer> fmap, int idx, String asf, int k){
+
+        if(k < 0) return;
+
+        if(idx == str.length()){
+          if(k == 0) System.out.println(asf);
+          return;
+        }
+
+        char ch = str.charAt(idx);
+
+        for(int i = fmap.get(ch); i >= 0; i--){
+          String part = "";
+          for(int j = 0; j < i; j++){
+            part += ch;
+          }
+          fun(str, fmap, idx + 1, asf + part, k - i);
+        }
+
+      }
+
+}
