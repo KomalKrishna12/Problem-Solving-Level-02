2c5b5dc85ce8b31ccc8cb2cf8ac7d09b16c32b32
KomalKrishna12 <komalkrishna016@gmail.com>
2022-03-25 22:46:40 +0530

    word k selection

--- Recursion/Q43_Words_k_selection_3.java
+++ Recursion/Q43_Words_k_selection_3.java
@@ -1,5 +1,7 @@
 import java.io.*;
 import java.util.*;
+// this is similar ques like Q26 but here we can take reptitive characters
+// so we use hashmap to store freq of each character
 public class Q43_Words_k_selection_3 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,29 +19,35 @@ public class Q43_Words_k_selection_3 {
           }
         }
     
-       fun(ustr, unique, 0, "", k);
+       fun(ustr, unique, 0, "", k, 0);
 
       }
 
-      public static void fun(String str, HashMap<Character, Integer> fmap, int idx, String asf, int k){
+      public static void fun(String str, HashMap<Character, Integer> fmap, int idx, String asf, int k, int ssf){
 
-        if(k < 0) return;
+        // when ssf(string so far) greater than k return 
+        if(ssf > k) return;
 
+        // when idx is at size of str then compare ssf and k if same then print and return
         if(idx == str.length()){
-          if(k == 0) System.out.println(asf);
+          if(ssf == k) System.out.println(asf);
           return;
         }
 
+        // store char at idx now check its freq
+        // if it is greater than 0 then reduce its freq by 1 and call for same idx bcoz
+        // ch is available in fmap and add ch into asf and add 1 to ssf
+        // one more call which will for not adding the char so we simpley increae idx by 1
         char ch = str.charAt(idx);
 
-        for(int i = fmap.get(ch); i >= 0; i--){
-          String part = "";
-          for(int j = 0; j < i; j++){
-            part += ch;
-          }
-          fun(str, fmap, idx + 1, asf + part, k - i);
+        if(fmap.get(ch) > 0){
+          fmap.put(ch, fmap.get(ch) - 1);
+          fun(str, fmap, idx, asf + ch, k, ssf + 1);
+          fmap.put(ch, fmap.get(ch) + 1);
         }
 
+        fun(str, fmap, idx + 1, asf, k, ssf);
+ 
       }
 
 }
