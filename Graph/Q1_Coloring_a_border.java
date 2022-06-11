29d0082699a8bc43879f294d01c41de5a58b2f51
KomalKrishna12 <komalkrishna016@gmail.com>
2022-06-11 11:11:00 +0530

    coloring a border

--- Graph/Q1_Coloring_a_border.java
+++ Graph/Q1_Coloring_a_border.java
@@ -1,5 +1,19 @@
+// we have given a 2D grid and given a row, col and a color
+// we have to color all connecetd elements of grid[row][col] with color given
+// suppose grid is {{1,1}, {1, 0}}, row = 0, col = 0, color = 3
+// so with (0,0) connecetd components are (0,1), (1,0), (1,1) so we color them with 3
+// so ans will be {{3,3}, {3,0}}
+// we have to color only borders
+// so we can use a variable count to store count of connectd component if count become 4 that means
+// it is not border so don't color it
 public class Q1_Coloring_a_border{
+    // this direction array will use to traverse in all four direction in order to get connecetd 
+    // components
     static int[][] dir = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+
+    // now win dfs function we pass color as grid[row][col] 
+    // so we mark conneccted vertex with -grid[row][col]
+    // so we can easily identify and change with the actual color
     public static int[][] colorBorder(int[][] grid, int row, int col, int color) {
         dfs(grid, row, col, grid[row][col]);
         for(int i = 0; i < grid.length; i++){
@@ -9,6 +23,12 @@ public class Q1_Coloring_a_border{
         }
         return grid;
     }
+
+    // firstly make it negetive
+    // create a count variavble to checl wheather it is corner or border
+    // now check all four directions
+    // if count become 4 that means it is not border so again convert it into previour means 
+    // make it positive
     public static void dfs(int[][] grid, int row, int col, int clr){
         grid[row][col] = -clr;
         int count = 0;
@@ -17,6 +37,8 @@ public class Q1_Coloring_a_border{
             int nrow = row + dir[i][0];
             int ncol = col + dir[i][1];
             
+            // check all it's corner points
+            // also check absolute value if it is not color then  continue
             if(nrow < 0 || ncol < 0 || nrow >= grid.length || ncol >= grid[0].length || Math.abs(grid[nrow][ncol]) != clr) 
                 continue;
             
