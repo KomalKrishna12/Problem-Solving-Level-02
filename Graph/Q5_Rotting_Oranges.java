f4218f6b3fc0626d9cd34c5f67797578bc66e099
KomalKrishna12 <komalkrishna016@gmail.com>
2022-06-12 19:07:53 +0530

    rotting oranges

--- Graph/Q5_Rotting_Oranges.java
+++ Graph/Q5_Rotting_Oranges.java
@@ -1,6 +1,10 @@
 import java.io.*;
 import java.util.ArrayDeque;
-
+// we have given a m*n grid in which three elements are there 0, 1, 2
+// 0 : empty cell, 1 : fresh orange, 2 : rotten orange
+// in one hour rotten orange start converting fresh orange into rotten orange
+// we have to rotten all four direction fresh orange and after all check if any rotten orange is 
+// present then return -1 else return level which is total hours taken
 public class Q5_Rotting_Oranges {
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -33,8 +37,17 @@ public class Q5_Rotting_Oranges {
 
     }
 
+    // all four directions in one array : up, right, down, left
     static int[][] dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
+    // create a variable fresh and count all fresh oranges (element 1)
+    // craete a Pair class in which row and col are their
+    // create a queue of Pair class, now add all rotten orange row and col
+    // now run a while loop till queue is empty
+    // store size in size variable, inside create another while loop and remove and check pair till size 
+    // become 0, using size we check level wise rotten oranges
+    // now check all four directions
+    // if any fresh orange(1) is their reduce fresh count add it's index into queue and mark it visited
     public static int orangesRotting(int[][] grid) {
         int fresh = 0;
         ArrayDeque<Pair> queue = new ArrayDeque<>();
