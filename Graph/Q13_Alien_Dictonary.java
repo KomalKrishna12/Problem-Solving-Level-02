e95bbdfee7cd9fb1a03f242051fdbb80ab1f88c0
KomalKrishna12 <komalkrishna016@gmail.com>
2022-06-15 15:09:09 +0530

    alien dictionary

--- Graph/Q13_Alien_Dictonary.java
+++ Graph/Q13_Alien_Dictonary.java
@@ -10,85 +10,70 @@ public class Q13_Alien_Dictonary {
       }
     
       public static String alienOrder(String[] words) {
-          // create two maps one for graph and one for indegree
-          // firstly set indegree as 0 for every char
-          HashMap<Character, HashSet<Character>> graph = new HashMap<>();
-          HashMap<Character, Integer> indegree = new HashMap<>();
-
-          for(String s : words){
-              for(char ch : s.toCharArray()){
-                  indegree.put(ch, 0);
+        HashMap<Character, HashSet<Character>> graph = new HashMap<>();
+        HashMap<Character, Integer> degree = new HashMap<>();
+    
+        for (String s : words) {
+          for (char ch : s.toCharArray()) {
+            degree.put(ch, 0);
+          }
+        }
+    
+        for (int i = 0; i < words.length - 1; i++) {
+          String curr = words[i];
+          String next = words[i + 1];
+          boolean flag = false;
+          int len = Math.min(curr.length(), next.length());
+          for (int j = 0; j < len; j++) {
+            char ch1 = curr.charAt(j);
+            char ch2 = next.charAt(j);
+            if (ch1 != ch2) {
+              HashSet<Character> set = new HashSet<>();
+              if (graph.containsKey(ch1) == true) {
+                set = graph.get(ch1);
+                if (set.contains(ch2) == false) {
+                  set.add(ch2);
+                  degree.put(ch2, degree.get(ch2) + 1);
+                  graph.put(ch1, set);
+                }
               }
-          }
-
-          // now compare two strings and add into graph
-          for(int i = 0; i < words.length - 1; i++){
-              String curr = words[i];
-              String next = words[i+1];
-              boolean flag = false;
-
-              // len will store min length so it'll compare till that 
-              int len = Math.min(curr.length(), next.length());
-
-              for(int j = 0; j < len; j++){
-                  char ch1 = curr.charAt(j);
-                  char ch2 = next.charAt(j);
-                  if(ch1 != ch2){
-                      // if char not matching that means here we can add that into graph so check
-                      // if that char is already their than add it into the previous set and increase 
-                      // indegree
-                      HashSet<Character> set = new HashSet<>();
-                      if(graph.containsKey(ch1) == true){
-                          set = graph.get(ch1);
-                          if(set.contains(ch2) == false){
-                              set.add(ch2);
-                              indegree.put(ch2, indegree.get(ch2) + 1);
-                              graph.put(ch2, set);
-                          }
-                      }
-                      else{
-                        set.add(ch2);
-                        indegree.put(ch2, indegree.get(ch2) + 1);
-                        graph.put(ch2, set);
-                      }
-                      flag = true;
-                      break;
-                  }
+              else {
+                set.add(ch2);
+                degree.put(ch2, degree.get(ch2) + 1);
+                graph.put(ch1, set);
               }
-
-              // suppose curr : abcd and next : abc
-              // here len = 3 and till 3 all chars are same but this is not same so return empty string
-              if(flag == false && curr.length() > next.length()) return "";
+              flag = true;
+              break;
+    
+            }
           }
-
-          // now create a queue and add all 0 indegree characters
-          // start removing charcaters and decrese their indegree by 1 and if indegree is 0 then
-          // add it into queue
-          ArrayDeque<Character> queue = new ArrayDeque<>();
-          StringBuilder sb = new StringBuilder();
-
-          for(char ch : indegree.keySet()){
-              if(indegree.get(ch) == 0)
-              queue.add(ch);
-          }
-
-          int count = 0;
-
-          while(queue.size() > 0){
-              char rem = queue.removeFirst();
-              count++;
-              sb.append(rem);
-              if(graph.containsKey(rem)){
-                  HashSet<Character> nbrs = graph.get(rem);
-                  for(char nbr : nbrs){
-                      indegree.put(nbr, indegree.get(nbr) - 1);
-                      if(indegree.get(nbr) == 0) 
-                      queue.add(nbr);
-                  }
-              }
-          }
-
-          if(count == indegree.size()) return sb.toString();
-          else return "";
+          if (flag == false && curr.length() > next.length()) return "";
+        }
+        
+        ArrayDeque<Character> queue = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder();
+        
+        for(char ch : degree.keySet()){
+            if(degree.get(ch) == 0) queue.add(ch);
+        }
+        
+        int count = 0;
+        
+        while(queue.size() > 0){
+            char rem = queue.removeFirst();
+            sb.append(rem);
+            count++;
+            if(graph.containsKey(rem) == true){
+                HashSet<Character> nbrs = graph.get(rem);
+                for(char nbr : nbrs){
+                    degree.put(nbr, degree.get(nbr) - 1);
+                    if(degree.get(nbr) == 0) queue.add(nbr);
+                }
+            }
+        }
+        
+        if(count == degree.size()) return sb.toString();
+        return "";
+        
       }
 }
