4515ad01f662b325ab08ed74735dc5dd5e247fe5
KomalKrishna12 <komalkrishna016@gmail.com>
2022-06-17 10:36:32 +0530

    negetive wight cycle

--- Graph/Q19_Negetive_weight_cycle_detection.java
+++ Graph/Q19_Negetive_weight_cycle_detection.java
@@ -1,56 +1,60 @@
 import java.io.*;
+
+// in Bellman ford we can find out shortest path till vertex v using v-1 iteartions
+// if we do one more iteration and any path value is reucing that means their exist a negetive
+// weight cycle
 public class Q19_Negetive_weight_cycle_detection {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] st = br.readLine().split(" ");
-    
-        int n = Integer.parseInt(st[0]);
-        int m = Integer.parseInt(st[1]);
-    
-        int[][] arr = new int[m][3];
-    
-        for (int i = 0; i < m; i++) {
-          st = br.readLine().split(" ");
-          arr[i][0] = Integer.parseInt(st[0]);
-          arr[i][1] = Integer.parseInt(st[1]);  
-          arr[i][2] = Integer.parseInt(st[2]);
-        }
-        
-        System.out.println(isNegetiveCyclePresent(n, arr));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] st = br.readLine().split(" ");
+
+		int n = Integer.parseInt(st[0]);
+		int m = Integer.parseInt(st[1]);
+
+		int[][] arr = new int[m][3];
+
+		for (int i = 0; i < m; i++) {
+			st = br.readLine().split(" ");
+			arr[i][0] = Integer.parseInt(st[0]);
+			arr[i][1] = Integer.parseInt(st[1]);
+			arr[i][2] = Integer.parseInt(st[2]);
+		}
+
+		System.out.println(isNegetiveCyclePresent(n, arr));
 	}
-	public static int isNegetiveCyclePresent(int n, int[][] arr){
-	    int[] path = new int[n];
-	    path[0] = 0;
-	    
-	    for(int i = 0; i < n - 1; i++){
-	        for(int j = 0; j < arr.length; j++){
-	            int u = arr[j][0];
-	            int v = arr[j][1];
-	            int wt = arr[j][2];
-	            
-	            if(path[u] == Integer.MAX_VALUE){
-	                continue;
-	            }
-	            
-	            if(path[u] + wt < path[v]){
-	                path[v] = path[u] + wt;
-	            }
-	        }
-	    }
-	    
-	    for(int j = 0; j < arr.length; j++){
-	            int u = arr[j][0];
-	            int v = arr[j][1];
-	            int wt = arr[j][2];
-	            
-	            if(path[u] == Integer.MAX_VALUE){
-	                continue;
-	            }
-	            
-	            if(path[u] + wt < path[v]){
-	                return 1;
-	            }
-	        }
-	        return 0;
+
+	public static int isNegetiveCyclePresent(int n, int[][] arr) {
+		int[] path = new int[n];
+		path[0] = 0;
+
+		for (int i = 0; i < n - 1; i++) {
+			for (int j = 0; j < arr.length; j++) {
+				int u = arr[j][0];
+				int v = arr[j][1];
+				int wt = arr[j][2];
+
+				if (path[u] == Integer.MAX_VALUE) {
+					continue;
+				}
+
+				if (path[u] + wt < path[v]) {
+					path[v] = path[u] + wt;
+				}
+			}
+		}
+
+		// vth iteration
+		// if any path is updating that means their is a negetive weight cycle so return
+		// 1 else return 0;
+		for (int j = 0; j < arr.length; j++) {
+			int u = arr[j][0];
+			int v = arr[j][1];
+			int wt = arr[j][2];
+
+			if (path[u] + wt < path[v]) {
+				return 1;
+			}
+		}
+		return 0;
 	}
 }
