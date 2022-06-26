4695a94729538171685c5048dbdd7aa9033fa3cc
KomalKrishna12 <komalkrishna016@gmail.com>
2022-06-26 10:09:31 +0530

    number of provinces

---
+++ Graph/Q31_Number_of_provinces.java
@@ -0,0 +1,28 @@
+public class Q31_Number_of_provinces {
+    public static int findCircleNum(int[][] graph) {
+        int ans = 0;
+        for(int i = 0; i < graph.length; i++){
+            if(graph[i][i] == 0) continue;
+            ans++;
+            dfs(graph, i);
+        }
+        return ans;
+    }
+
+    public static void dfs(int[][] graph, int i){
+        graph[i][i] = 0;
+        for(int j = 0; j < graph.length; j++){
+            if(graph[i][j] == 1){
+                graph[i][j] = 0;
+                if(graph[j][j] == 1){
+                    dfs(graph, j);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] isConnected = {{1,1,0},{1,1,0},{0,0,1}};
+        System.out.println(findCircleNum(isConnected));
+    }
+}
